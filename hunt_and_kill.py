73ea038c55f44749f998192377d59cf36069d5fb
chenandr <chenandr@umich.edu>
2021-01-10 14:06:19 -0500

    hunt and kill gif implementation

--- hunt_and_kill.py
+++ hunt_and_kill.py
@@ -17,7 +17,7 @@ def local_wall_idx(dir):
     elif dir == 'B':
         return 3
 
-def walk(grid, cur_cell):
+def walk(grid, cur_cell, gif):
     #Check neighbors
     grid[cur_cell[0]][cur_cell[1]].visited = True
     
@@ -30,18 +30,25 @@ def walk(grid, cur_cell):
             valid_nbrs.append((idx, dir))
     
     if(len(valid_nbrs) == 0):
+        if gif:
+            newIMG = util.create_snapshot(gif_arr[-1].copy(), (cur_cell[0] * 2 + 1, cur_cell[1] * 2 + 1), -1)
+            gif_arr.append(newIMG)
         return
     
+    #Select random valid neighbor to walk to next
     x = random.randint(0, len(valid_nbrs) - 1)
-    
     dir = valid_nbrs[x][1]
     wall_idx = local_wall_idx(dir)
     
+    if gif:
+        newIMG = util.create_snapshot(gif_arr[-1].copy(), (cur_cell[0] * 2 + 1, cur_cell[1] * 2 + 1), wall_idx)
+        gif_arr.append(newIMG)
+    
     grid[cur_cell[0]][cur_cell[1]].walls[wall_idx] = 'X'
     
-    walk(grid, valid_nbrs[x][0])
+    walk(grid, valid_nbrs[x][0], gif)
 
-def hunt(grid):
+def hunt(grid, gif):
     for r in range(Rows):
         for c in range(Cols):
             if(not grid[r][c].visited):
@@ -55,15 +62,19 @@ def hunt(grid):
                         valid_nbrs.append(dir)
                
                 if len(valid_nbrs):
+                    #Choose random neighbor in maze to connect to
                     x = random.randint(0, len(valid_nbrs) - 1)
                     wall_idx = local_wall_idx(valid_nbrs[x])
+                    
+                    if gif:
+                        newIMG = util.create_snapshot(gif_arr[-1].copy(), (r * 2 + 1, c * 2 + 1), wall_idx)
+                        gif_arr.append(newIMG)
+                        
                     grid[r][c].walls[wall_idx] = 'X'
                     return (r,c)
     return None
 
 def hunt_and_kill(rows, cols, gif):
-    random.seed(0)
-    
     grid = [[node(['L','R','T','B']) for j in range(cols)] for i in range(rows)]
     
     global Directions 
@@ -72,7 +83,6 @@ def hunt_and_kill(rows, cols, gif):
     Rows = rows
     global Cols
     Cols = cols
-    
     #Choose random start, perform random walk until no unvisited neighbors of current cell --> Deadend
     #Then Enter Hunt Mode and find first unvisited cell with visited neighbors
     #Carve a passage from that cell to a random visited neigbor and begin a new walk
@@ -80,14 +90,28 @@ def hunt_and_kill(rows, cols, gif):
     rand_c = random.randint(0,cols-1)
     cell = (rand_r, rand_c)
     
+    global gif_arr
+    gif_arr = []
+    if gif:
+        maze = np.zeros(((2 * rows) + 1, (2 * cols) + 1), dtype=np.uint8)
+        gif_arr.append(maze)
+        newIMG = util.create_snapshot(gif_arr[-1], (rand_r * 2 + 1, rand_c * 2 + 1), -1)
+        gif_arr.append(newIMG)
+    
     while cell != None:
-        walk(grid, cell)
-        cell = hunt(grid)
+        walk(grid, cell, gif)
+        cell = hunt(grid, gif)
     
     x = random.randint(0, cols - 1)
     grid[0][x].walls[2] = 'X'
     
-    x = random.randint(0, cols - 1)
-    grid[len(grid) - 1][x].walls[3] = 'X'
+    y = random.randint(0, cols - 1)
+    grid[len(grid) - 1][y].walls[3] = 'X'
+    
+    if gif:
+        newIMG = util.create_snapshot(gif_arr[-1].copy(), (0, x * 2 + 1), -1)
+        newIMG = util.create_snapshot(newIMG, (rows * 2, y * 2 + 1), -1) 
+        gif_arr.append(newIMG)
+        return gif_arr
     
     return grid
